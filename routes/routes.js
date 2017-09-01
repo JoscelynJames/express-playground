ddc397f3c812914f3b92f8ca8bb6eb0576bcfc97
Joscelyn James <josce.james7@hotmail.com>
2017-08-31 22:01:50 -0600

    Finished 03 Volume with Path Variables

--- routes/routes.js
+++ routes/routes.js
@@ -4,18 +4,18 @@ const router = express.Router();
 var pinum = Math.PI;
 
 router.get('/', function(request, response) {
-  response.send('<h1>Hello</h1><h2>Welcome to my Express-Playground</h2>')
+  response.send('<h1>Hello</h1><h2>Welcome to my Express-Playground</h2>');
 });
 
 router.get('/math', function(request, response) {
-  response.send('<h1>Hello</h1><h2>Welcome to Math</h2>')
+  response.send('<h1>Hello</h1><h2>Welcome to Math</h2>');
 });
 
 router.get('/math/pi', function(request, response) {
   response.send('Pi = ' + pinum);
 });
 
-router.use('/math/calculate', function(req, res) {
+router.get('/math/calculate', function(req, res) {
   var operation = req.query.operation;
   var x = parseInt(req.query.x);
   var y = parseInt(req.query.y);
@@ -26,13 +26,20 @@ router.use('/math/calculate', function(req, res) {
   } else if (operation === 'subtract'){
     total = x - y;
   } else if (operation === 'mutliply') {
-    total = x * y;
-  } else {
+    total = (x * y);
+  } else if (operation === 'divide') {
     total = x / y;
   };
-
-  console.log( x ,typeof operation, y, typeof total);
+console.log(req.query);
    res.send('Total = ' + total);
 });
 
+router.use('/math/volume/:length/:width/:height', function(req, res) {
+  var length = req.params.length;
+  var width = req.params.width;
+  var height = req.params.height;
+  var volume = length * width * height;
+  
+  res.send(`The volume of a ${length}x${width}x${height} rectangle is ${volume}`)
+})
 module.exports = router;
